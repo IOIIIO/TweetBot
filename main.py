724450c2baccb96a67265f6d8a7186022331a084
J P <owner@Js-MacBook-Pro.local>
2020-07-29 19:30:22 +0200

    Add image upload

--- main.py
+++ main.py
@@ -14,21 +14,46 @@ from creds import (
     access_token,
     access_token_secret,
     messages,
-    delay
+    delay,
+    photo
 )
 
+q = ""
 st = time.time()
 t = Twython(consumer_key,consumer_secret,access_token,access_token_secret)
 
 while True:
     try: 
+        # perf counter start
         tic = time.perf_counter()
-        w = random.randint(0, 12345678)
+        # Pick random message
+        k = random.choice(messages)
+        # Generate random number and append
+        w = random.randint(0, 12345678) # This will eventually cause us to create duplicates.
         r = " [{}]".format(w)
-        n = random.choice(messages) + r
-        t.update_status(status=n)
+        n = k + r
+        # Grab image if message has one and upload
+        if k in photo:
+            try:
+                d = open(photo[k], 'rb')
+                try:
+                    q = t.upload_media(media=d)
+                except Exception as e:
+                    print("Failed to upload image. Exception: {}".format(e))
+            except Exception as e: 
+                print("Failed to open image. Exception: {}".format(e))
+        # Send tweet
+        if q == "":
+            t.update_status(status=n)
+            z = "with image"
+        else:
+            t.update_status(status=n, media_ids=[q['media_id']])
+            z = ""
+        # perf counter end
         toc = time.perf_counter()
-        print("Tweeted: {} in {} seconds".format(n, toc-tic))
+        print("Tweeted: {} in {} seconds {}".format(n, toc-tic, z))
     except Exception as e:
         print(e)
+    # Wait before running again
+    q = ""
     time.sleep(float(delay) - ((time.time() - st) % float(delay)))
